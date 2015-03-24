bbadd1ea3a68bdef7bce73d138fc322701e70f9b
zubietaroberto <zubieta.roberto.e@gmail.com>
2015-03-24 12:27:42 -0500

    Basic Material Design

--- app/src/main/java/pa/com/poroto/pktest/PrivateKeyActivity.java
+++ app/src/main/java/pa/com/poroto/pktest/PrivateKeyActivity.java
@@ -1,12 +1,12 @@
 package pa.com.poroto.pktest;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class PrivateKeyActivity extends Activity {
+public class PrivateKeyActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/pa/com/poroto/pktest/PrivateKeyFragment.java
+++ app/src/main/java/pa/com/poroto/pktest/PrivateKeyFragment.java
@@ -1,7 +1,7 @@
 package pa.com.poroto.pktest;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
